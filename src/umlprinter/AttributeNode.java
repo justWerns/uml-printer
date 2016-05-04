17612f68000e5a0a3dde9610d6e76574d079389a
Andrew Wernicki <awern214@live.kutztown.edu>
2016-05-03 22:49:35 -0400

    Added printUMLJaws to AttributeNode.

--- src/umlprinter/AttributeNode.java
+++ src/umlprinter/AttributeNode.java
@@ -26,4 +26,19 @@ public class AttributeNode extends AbstractNode {
         }
         return out.toString();
     }
+
+    @Override
+    public String printUMLJaws()
+            throws InvocationTargetException, NoSuchMethodException,
+                   InstantiationException, IllegalAccessException {
+        StringBuilder out = new StringBuilder();
+        for (Object obj : parsetree.subList(1, parsetree.size())) {
+            if (obj instanceof String)
+                out.append(obj);
+            else
+                out.append(AbstractNode.buildNode(
+                    (List) obj).printUMLJaws());
+            }
+        return out.toString();
+    }
 } // end AttributeNode
